87438bfafc6458d18c1463c57f097e9f962bd919
shakeel <shakeel786@gmail.com>
2024-10-04 22:28:43 +0500

    first comment

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div  name="about"
+        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'
+    >
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'
+        >
+            <div className='pb-8'>
+                <p className='text-4xl font-bold text-center mb-12'>About</p>
+                <p className='text-xl mt-20'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                  Laboriosam molestias omnis id ullam. 
+                  Culpa distinctio quas amet excepturi ullam 
+                  aperiam libero magni debitis eius. 
+                  Reiciendis, nihil! Dicta in, adipisci maxime rerum
+                  incidunt aspernatur et ex, totam veniam 
+                  repellat mollitia sunt corrupti quas 
+                  aliquid ipsa amet ipsam nisi enim asperiores 
+                  minima!
+                </p>
+                <br />
+                <p className='text-xl'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                  Doloremque ex necessitatibus quasi impedit 
+                  eius modi unde! Nihil impedit maxime eveniet harum 
+                  illum necessitatibus, minus, corrupti, aliquam laudantium
+                   porro molestiae consequatur qui. Laboriosam 
+                   illum soluta, sed eum aspernatur, nisi quo tempora 
+                   provident hic, molestiae saepe sint facere odit minus 
+                   reiciendis consequatur?
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Experience.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import js from "../assets/js.png";
+import react from "../assets/react.png";
+import tailwind from "../assets/tailwind.png";
+import github from "../assets/github.png";
+export const Experience = () => {
+    const techs = [
+        {
+            id:1,
+            src:html,
+            title:'HTML',
+            style:'shadow-orange-500',
+        },
+        {
+            id:2,
+            src:css,
+            title:'CSS',
+            style:'shadow-blue-500',
+        },
+        {
+            id:3,
+            src:js,
+            title:'JS',
+            style:'shadow-yellow-500',
+        },
+        {
+            id:4,
+            src:react,
+            title:'React',
+            style:'shadow-blue-600',
+        },
+        {
+            id:5,
+            src:tailwind,
+            title:'Tailwind',
+            style:'shadow-sky-400',
+        },
+        {
+            id:6,
+            src:github,
+            title:'GitHub',
+            style:'shadow-gray-400',
+        },
+        
+    ];
+  return (
+    <div 
+    name="experience" 
+    ClassName="bg-gradient-to-b from-black to-gray-800 w-full h-full justify-cente"
+    >
+        <div className="
+        justify-center bg-gradient-to-b from-black to-gray-800 w-full h-full text-white">
+            <div className='text-4xl  text-center mb-12'> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                <p className='font-bold'>Experience</p>
+                <p className='py-6'>These are the technologies I've worked with</p>
+            </div>
+
+
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+{
+    techs.map(({id, src, title, style}) => (
+<div 
+key={id}
+
+className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                    <img src={src} alt="" className='w-20 mx-auto' />
+                    <p className='mt-4'>{title}</p>
+                </div>
+    )
+    )
+}
+
+
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Experience
